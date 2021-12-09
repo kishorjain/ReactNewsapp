e2f5e8e99779080d090951a848ba8568b968281c
Kishor <kishor.jain025@gmail.com>
2021-12-10 02:45:38 +0530

    Add author,date,source

--- src/components/News.js
+++ src/components/News.js
@@ -62,12 +62,12 @@ export class News extends Component {
     render() {
         return (
             <div className="container">
-                <h5 className="my-3 text-center">News Monkey Headlines</h5>
+                <h5 className="my-3 text-center">News Headlines</h5>
                 {this.state.loading && <Spinner />}
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element) => {
                         return <div className="col-md-4 mt-3" key={element.url} >
-                            <NewsItem title={element.title ? element.title : "No News"} description={element.description ? element.description : "No News"} imageUrl={element.urlToImage} newsUrl={element.url} />
+                            <NewsItem title={element.title ? element.title : "No News"} description={element.description ? element.description : "No News"} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} publishedDate={element.publishedAt} source={element.source.name}/>
                         </div>
 
                     })}
