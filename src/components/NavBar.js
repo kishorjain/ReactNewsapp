86620fef58e3a67d64878e1e4f175a26e933da49
Kishor <kishor.jain025@gmail.com>
2022-01-01 01:10:51 +0530

    Navbar changes to function based approch

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {
     Link,
   } from "react-router-dom";
 //import PropTypes from 'prop-types'
 
-export class NavBar extends Component {
+const NavBar =()=> {
     //static propTypes = {
 
     //}
 
-    render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
                     <div className="container-fluid">
                         <Link className="navbar-brand" to="/">News App</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -41,7 +40,7 @@ export class NavBar extends Component {
 
             </div>
         )
-    }
+    
 }
 
 export default NavBar
